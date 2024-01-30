7e01a17fcf6d30bdbbba84a910c68fdbe18d1da8
Nick20010505 <nick24564625@gmail.com>
2024-01-30 12:28:39 +0800

    0129

--- 自己練習/autoencoder_實作.py
+++ 自己練習/autoencoder_實作.py
@@ -77,6 +77,7 @@ plt.ion()   # continuously plot
 view_data = train_data.train_data[:N_TEST_IMG].view(-1, 28*28).type(torch.FloatTensor)/255.
 for i in range(N_TEST_IMG):
     a[0][i].imshow(np.reshape(view_data.data.numpy()[i], (28, 28)), cmap='gray'); a[0][i].set_xticks(()); a[0][i].set_yticks(())
+view_data = view_data.to(device)
 
 # training and testing
 for epoch in range(EPOCH):
@@ -96,22 +97,13 @@ for epoch in range(EPOCH):
             print('Epoch: ', epoch, '| train loss: %.4f' % loss.item())
 
             # plotting decoded image (second row)
+            
             _, decoded_data = autoencoder(view_data)
             for i in range(N_TEST_IMG):
                 a[1][i].clear()
-                a[1][i].imshow(np.reshape(decoded_data.data.numpy()[i], (28, 28)), cmap='gray')
+                a[1][i].imshow(np.reshape(decoded_data.cpu().data.numpy()[i], (28, 28)), cmap='gray')
                 a[1][i].set_xticks(()); a[1][i].set_yticks(())
             plt.draw(); plt.pause(0.05)
-plt.ioff()
-plt.show()
 
-# visualize in 3D plot
-view_data = train_data.train_data[:200].view(-1, 28*28).type(torch.FloatTensor)/255.
-encoded_data, _ = autoencoder(view_data)
-fig = plt.figure(2); ax = Axes3D(fig)
-X, Y, Z = encoded_data.data[:, 0].numpy(), encoded_data.data[:, 1].numpy(), encoded_data.data[:, 2].numpy()
-values = train_data.train_labels[:200].numpy()
-for x, y, z, s in zip(X, Y, Z, values):
-    c = cm.rainbow(int(255*s/9)); ax.text(x, y, z, s, backgroundcolor=c)
-ax.set_xlim(X.min(), X.max()); ax.set_ylim(Y.min(), Y.max()); ax.set_zlim(Z.min(), Z.max())
+plt.ioff()
 plt.show()
